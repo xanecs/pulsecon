951827ce2b10fd59df3b2375451ab16b2aa79e41
Leon Bentrup <leon@leonadi.de>
2017-04-04 21:38:40 +0200

    Add volume control

--- pulsecon/pulsecon.py
+++ pulsecon/pulsecon.py
@@ -3,44 +3,69 @@ import redis
 import toml
 import json
 
-def update_list(pulse):
-    inputs = pulse.sink_input_list()
-    dict = {}
-    for input in inputs:
-        dict[input.proplist['application.name']] = input.__dict__['buffer_usec'] > 0
-    return dict
+class Pulsecon(object):
+    def update_list(self):
+        inputs = self.pulse.sink_input_list()
+        newstate = {}
+        for input in inputs:
+            newstate[input.proplist['application.name']] = input.__dict__['buffer_usec'] > 0
+        if newstate != self.playstate:
+            self.r.publish(self.redis_channel + ':playing', json.dumps(newstate))
+            for k in newstate:
+                self.r.set(self.redis_channel + ':' + k, 'true' if newstate[k] else 'false')
+            for k in self.playstate:
+                if not (k in newstate):
+                    self.r.set(self.redis_channel + ':playing:' + k, 'false')
+            self.playstate = newstate
 
-def init():
-    playstate = None
-    with open('config.toml') as conffile:
-        config = toml.loads(conffile.read())
+    def update_volume(self):
+        sinks = self.pulse.sink_list()
+        volume = self.pulse.volume_get_all_chans(sinks[0])
+        if self.volume != volume:
+            self.r.publish(self.redis_channel + ':volume', json.dumps({'volume': volume}))
+            self.r.set(self.redis_channel + ':volume', str(volume))
+            self.volume = volume
 
-    redis_host = config['redis']['host']
-    redis_port = config['redis']['port']
-    redis_channel = config['redis']['channel']
+    def set_volume(self, volume):
+        sinks = self.pulse.sink_list()
+        self.pulse.volume_set_all_chans(sinks[0], volume)
 
-    r = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+    def handle_command(self, message):
+        data = json.loads(message['data'].decode('utf-8'))
+        if 'action' in data and data['action'] == 'volume':
+            self.set_volume(data['params']['volume'])
 
-    with Pulse('pulsecon') as pulse:
-        def callback(ev):
-            print(ev)
-            print('---')
-            print(ev.__dict__)
-            raise PulseLoopStop
+    def __init__(self):
+        self.playstate = {}
+        self.volume = None
 
-        pulse.event_mask_set('all')
-        pulse.event_callback_set(callback)
-        while(True):
-            try:
-                pulse.event_listen(timeout=1)
-                newstate = update_list(pulse)
-                if newstate != playstate:
-                    playstate = newstate
-                    r.publish(redis_channel, json.dumps(playstate))
-                    for k in playstate:
-                        r.set(redis_channel + ':' + k, 'true' if playstate[k] else 'false')
-            except KeyboardInterrupt:
-                break
+        with open('config.toml') as conffile:
+            config = toml.loads(conffile.read())
+
+        redis_host = config['redis']['host']
+        redis_port = config['redis']['port']
+        self.redis_channel = config['redis']['channel']
+
+        self.r = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+        self.p = self.r.pubsub()
+        self.p.subscribe(**{self.redis_channel + ':command': self.handle_command}, ignore_subscribe_messages=True)
+
+        with Pulse('pulsecon') as pulse:
+            self.pulse = pulse
+            def callback(ev):
+                raise PulseLoopStop
+
+            pulse.event_mask_set('all')
+            pulse.event_callback_set(callback)
+            while(True):
+                try:
+                    pulse.event_listen(timeout=0.2)
+                    self.update_list()
+                    self.update_volume()
+                    self.p.get_message()
+
+                except KeyboardInterrupt:
+                    break
 
 if __name__ == '__main__':
-    init()
+    Pulsecon()
